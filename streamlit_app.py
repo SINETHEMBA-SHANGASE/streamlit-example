e21f3424403ec1b0b7b015915c4e25b21f2f97f6
SINETHEMBA-SHANGASE <136069091+SINETHEMBA-SHANGASE@users.noreply.github.com>
2023-07-31 13:29:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import pandas
 
 streamlit.title('My Parent New Healthy Diner')
 
+
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text ('🥗 Kale, Spinach & Rocket Smoothie')
